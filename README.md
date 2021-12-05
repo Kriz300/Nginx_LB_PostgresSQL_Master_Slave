c04a5fb7e147c0ac67ebd0920152d8069dfb2b0f
Kriz300 <christian.munoz1@mail.udp.cl>
2021-12-05 19:57:56 -0300

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# En Desarrollo
+
+_._
+
+## Ejecución 🔧
+
+
+
+### Comandos:
+
+
+
+## Estructura 🛠️
+
+### Construido con:
+
+**Javascript**
+
+
+## Autor ✒️
+
+* **Christian Muñoz I.** [Kriz](https://github.com/Kriz300)
+* **Camilo Rubilar** [Niyet](https://github.com/niyetsin)
+* **Raimundo Perez** [Raimundo Perez](https://github.com/raimundoperez8)
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia MIT - mira el archivo [LICENSE](LICENSE) para detalles.
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, url_for, request
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+conn_master= "host='localhost' dbname='my_database' user='user2' password='password2' port='55432'"
+master = psycopg2.connect(conn_master)
+
+conn_slave = "host='localhost' dbname='my_database' user='user2' password='password2' port='65432'"
+slave = psycopg2.connect(conn_slave)
+
+@app.route("/hello")
+def hello_world():
+    return "<p>PID: " + str(os.getpid()) + "</p>"
+
+@app.route("/GetProduct/<name>",methods = ['GET'])
+def get(name):
+    cur = slave.cursor()
+    query = "select * from product where lower(product.name) like '%{}%';".format(name.lower())
+    cur.execute(query)
+    response = cur.fetchall()
+    cur.close()
+
+    return "<p>PID: " + str(response) + "</p>"
+
+@app.route("/AddProduct",methods = ['POST'])
+def add():
+    return "<p>PID: " + request.form['product'] + "</p>"
